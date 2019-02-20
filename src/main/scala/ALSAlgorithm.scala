731a74596aa9aef8201f43240acb69382f4192ae
vinay <vinay.naik@outlook.com>
2019-02-20 14:44:11 +0530

    eliminate batch predict, calibrate algorithm for implicit als

--- src/main/scala/ALSAlgorithm.scala
+++ src/main/scala/ALSAlgorithm.scala
@@ -15,6 +15,8 @@ import org.apache.spark.mllib.recommendation.ALSModel
 
 import grizzled.slf4j.Logger
 
+import scala.collection.mutable.ArrayBuffer
+
 case class ALSAlgorithmParams(
   rank: Int,
   numIterations: Int,
@@ -34,47 +36,73 @@ class ALSAlgorithm(val ap: ALSAlgorithmParams)
   }
 
   def train(sc: SparkContext, data: PreparedData): ALSModel = {
-    // MLLib ALS cannot handle empty training data.
-    require(!data.ratings.take(1).isEmpty,
-      s"RDD[Rating] in PreparedData cannot be empty." +
+    require(!data.viewEvents.take(1).isEmpty,
+      s"viewEvents in PreparedData cannot be empty." +
       " Please check if DataSource generates TrainingData" +
-      " and Preparator generates PreparedData correctly.")
-    // Convert user and item String IDs to Int index for MLlib
-
-    val userStringIntMap = BiMap.stringInt(data.ratings.map(_.user))
-    val itemStringIntMap = BiMap.stringInt(data.ratings.map(_.item))
+      " and Preprator generates PreparedData correctly.")
+    require(!data.users.take(1).isEmpty,
+      s"users in PreparedData cannot be empty." +
+      " Please check if DataSource generates TrainingData" +
+      " and Preprator generates PreparedData correctly.")
+    require(!data.items.take(1).isEmpty,
+      s"items in PreparedData cannot be empty." +
+      " Please check if DataSource generates TrainingData" +
+      " and Preprator generates PreparedData correctly.")
+      
+      println("Model training for ALS view only initiated")
+      
+      // create User and item's String ID to integer index BiMap
+    val userStringIntMap = BiMap.stringInt(data.users.keys)
+    val itemStringIntMap = BiMap.stringInt(data.items.keys)
     
     // collect Item as Map and convert ID to Int index
-    val items: Map[Int, Item] = data.items.map { case (id, item) =>
-      (itemStringIntMap(id), item)
+    val items: Map[Int, Item] = data.items.map { case (id, item) =>(itemStringIntMap.getOrElse(id, 0), item)
+      case default => (0, Item("00000000-0000-0000-0000-000000000000", None,"haystack.in","POV"))
     }.collectAsMap.toMap
     
-    val mllibRatings = data.ratings.map( r =>
-      // MLlibRating requires integer index for user and item
-      MLlibRating(userStringIntMap(r.user), itemStringIntMap(r.item), r.rating)
-    )
+    val mllibRatings = data.viewEvents
+      .map { r =>
+        // Convert user and item String IDs to Int index for MLlib
+        val uindex = userStringIntMap.getOrElse(r.user, -1)
+        val iindex = itemStringIntMap.getOrElse(r.item, -1)
+
+        if (uindex == -1)
+          logger.info(s"Couldn't convert nonexistent user ID ${r.user}"
+            + " to Int index.")
+
+        if (iindex == -1)
+          logger.info(s"Couldn't convert nonexistent item ID ${r.item}"
+            + " to Int index.")
+
+        ((uindex, iindex), 1)
+      }.filter { case ((u, i), v) =>
+        // keep events with valid user and item index
+        (u != -1) && (i != -1)
+      }.reduceByKey(_ + _) // aggregate all view events of same user-item pair
+      .map { case ((u, i), v) =>
+        // MLlibRating requires integer index for user and item
+        MLlibRating(u, i, v)
+      }
+      .cache()
+
+    // MLLib ALS cannot handle empty training data.
+    require(!mllibRatings.take(1).isEmpty,
+      s"mllibRatings cannot be empty." +
+      " Please check if your events contain valid user and item ID.")
 
     // seed for MLlib ALS
     val seed = ap.seed.getOrElse(System.nanoTime)
-
-    // Set checkpoint directory
-    // sc.setCheckpointDir("checkpoint")
-
-    // If you only have one type of implicit event (Eg. "view" event only),
-    // set implicitPrefs to true
-    // MODIFIED
-    val implicitPrefs = true
-    val als = new ALS()
-    als.setUserBlocks(-1)
-    als.setProductBlocks(-1)
-    als.setRank(ap.rank)
-    als.setIterations(ap.numIterations)
-    als.setLambda(ap.lambda)
-    als.setImplicitPrefs(implicitPrefs)
-    als.setAlpha(1.0)
-    als.setSeed(seed)
-    als.setCheckpointInterval(10)
-    val m = als.run(mllibRatings)
+    
+    val m = ALS.trainImplicit(
+      ratings = mllibRatings,
+      rank = ap.rank,
+      iterations = ap.numIterations,
+      lambda = ap.lambda,
+      blocks = -1,
+      alpha = 1.0,
+      seed = seed)
+    
+    println("Model through ALS view only training complete")
 
     new ALSModel(
       rank = m.rank,
@@ -86,64 +114,53 @@ class ALSAlgorithm(val ap: ALSAlgorithmParams)
   }
 
   def predict(model: ALSModel, query: Query): PredictedResult = {
-    model.items.withDefaultValue(new Item(None, "haystack.in", "POV"))
-    // Convert String ID to Int index for Mllib
-    model.userStringIntMap.get(query.user).map { userInt =>
-      // create inverse view of itemStringIntMap
+    model.items.withDefaultValue(new Item("00000000-0000-0000-0000-000000000000", None, "haystack.in", "POV"))
+    
+    // convert items to Int index
+    val queryList: Set[Int] = query.users.map(model.itemStringIntMap.get(_))
+      .flatten.toSet
+    // get all items
+    val allItemsMap = model.items
+    
+    var combinedWithOthers = ArrayBuffer[ItemScore]()
+    queryList.foreach (e => {
+      println("user now is: " + e)
       val itemIntStringMap = model.itemStringIntMap.inverse
-      // recommendProducts() returns Array[MLlibRating], which uses item Int
-      // index. Convert it to String ID for returning PredictedResult
-      val itemScores = model.recommendProducts(userInt, query.num)
-        .map (r => ItemScore(itemIntStringMap(r.product), r.rating, model.items(r.product).domain, model.items(r.product).itemType))
-      PredictedResult(itemScores)
-    }.getOrElse{
-      logger.info(s"No prediction for unknown user ${query.user}.")
-      PredictedResult(Array.empty)
-    }
+      println("triggering item recommendations")
+      try{
+        val itemScores = model.recommendProducts(e, query.num)
+           .map (r => ItemScore(itemIntStringMap(r.product), r.rating, allItemsMap(r.product).domain, allItemsMap(r.product).itemType))
+        combinedWithOthers = combinedWithOthers ++ itemScores
+      } catch {
+        case ex : NoSuchElementException => {
+            //println("No user features found for this item")
+         }
+        case e: Exception => {
+            //println("No user features found for this item")
+        }
+      }
+    })
+    
+
+    
+    /*query.items.foreach (e => {
+      // Convert String ID to Int index for Mllib
+       model.itemStringIntMap.get(e).map { itemInt =>
+         // create inverse view of userStringIntMap
+         val userIntStringMap = model.userStringIntMap.inverse
+         // recommendUsers() returns Array[MLlibRating], which uses item Int
+         // index. Convert it to String ID for returning PredictedResult
+         val userScores = model.recommendUsers(itemInt, query.num)
+         .map (r => UserScore(userIntStringMap(r.user), r.rating))
+         combinedWithOthers ++ userScores
+       }.getOrElse{
+        logger.info(s"No prediction for unknown item ${e}.")
+       }
+       println("what is being combined")
+       combinedWithOthers.take(50).foreach(println)
+    })*/
+    
+    PredictedResult(combinedWithOthers.toArray)
   }
 
-  // This function is used by the evaluation module, where a batch of queries is sent to this engine
-  // for evaluation purpose.
-  override def batchPredict(model: ALSModel, queries: RDD[(Long, Query)]): RDD[(Long, PredictedResult)] = {
-    val userIxQueries: RDD[(Int, (Long, Query))] = queries
-    .map { case (ix, query) => {
-      // If user not found, then the index is -1
-      val userIx = model.userStringIntMap.get(query.user).getOrElse(-1)
-      (userIx, (ix, query))
-    }}
-
-    // Cross product of all valid users from the queries and products in the model.
-    val usersProducts: RDD[(Int, Int)] = userIxQueries
-      .keys
-      .filter(_ != -1)
-      .cartesian(model.productFeatures.map(_._1))
-
-    // Call mllib ALS's predict function.
-    val ratings: RDD[MLlibRating] = model.predict(usersProducts)
-
-    // The following code construct predicted results from mllib's ratings.
-    // Not optimal implementation. Instead of groupBy, should use combineByKey with a PriorityQueue
-    val userRatings: RDD[(Int, Iterable[MLlibRating])] = ratings.groupBy(_.user)
-
-    userIxQueries.leftOuterJoin(userRatings)
-    .map {
-      // When there are ratings
-      case (userIx, ((ix, query), Some(ratings))) => {
-        val topItemScores: Array[ItemScore] = ratings
-        .toArray
-        .sortBy(_.rating)(Ordering.Double.reverse) // note: from large to small ordering
-        .take(query.num)
-        .map { rating => ItemScore(
-          model.itemStringIntMap.inverse(rating.product),
-          rating.rating, "", "") }
-
-        (ix, PredictedResult(itemScores = topItemScores))
-      }
-      // When user doesn't exist in training data
-      case (userIx, ((ix, query), None)) => {
-        require(userIx == -1)
-        (ix, PredictedResult(itemScores = Array.empty))
-      }
-    }
-  }
 }
